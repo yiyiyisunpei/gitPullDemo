6ddfa45dc80b4af2418b05a49a857efbc519a909
sunpei <sunpei123>
2021-12-27 10:39:37 +0800

    Initial commit

---
+++ src/main/java/GitTest.java
@@ -0,0 +1,11 @@
+/**
+ * @author sunpei
+ * @version 1.0
+ * @date 2021/12/27
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("模拟一条数据上传到github");
+    }
+
+}
